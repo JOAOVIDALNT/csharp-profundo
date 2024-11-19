bf55bcdb3ee7382192237a573a985d3926f141e2
João Vidal <dev.joaovidal@gmail.com>
2024-11-19 19:05:33 -0300

    att

---
+++ day-001/challange-001/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+static int CountBits(int n = 4)
+{
+    decimal dec = Convert.ToDecimal(n);
+
+    var array = decimal.GetBits(dec);
+
+    var count = 0;
+
+    foreach (var bit in array)
+    {
+        if (bit == 1)
+            count++;
+    }
+
+    return count;
+}
+
+Console.WriteLine(CountBits());
